f438c950a27d6213ec7e8893e2c9dca6cbe56fa4
garbsrmartins <116748073+garbsrmartins@users.noreply.github.com>
2022-11-20 10:43:04 -0300

    asd

--- src/Cards/CardsContato.jsx
+++ src/Cards/CardsContato.jsx
@@ -9,43 +9,20 @@ const useStyles = makeStyles ((theme) => ({
 cardsDiv: {
 
     minHeight: '100vh',
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/4.webp'})` ,
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/pool.webp'})` ,
     backgroundRepeat: 'no-repeat',
     
     backgroundSize: 'cover',
 },
 
-cardsDiv1: {
 
-    minHeight: '100vh',
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/21.webp'})` ,
-    backgroundRepeat: 'no-repeat',
-   
-    backgroundSize: 'cover',
-},
-cardsDiv2: {
-
-    minHeight: '100vh',
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/tone1.webp'})` ,
-    backgroundRepeat: 'no-repeat',
-   
-    backgroundSize: 'cover',
-},
-
-cards1: {
-
-    width: '100%',
-
-      
-    
-},
 
 cards: {
     width: '100%',
     backgroundSize: 'cover',
-    background: '#00000028 ',
+    background: '#ffffff40 ',
     
-    height:'100vh',
+ 
 
     "@media (max-width: 1280px)": {
         display: 'flex',
@@ -58,8 +35,8 @@ cards: {
 
 texto: {
 
-    
-color: 'white',
+marginTop: '100px',   
+color: 'black',
 fontFamily: 'Montserrat',
 display: 'inline-block',
 fontSize: '3em',
@@ -87,26 +64,18 @@ verticalAlign: '-60vh',
 
 description: {
     
-color: 'white',
+color: 'black',
 fontFamily: 'Montserrat',
 margin: '2em',
 fontSize: '0.7em',
 
 textAlign: 'center',
-display: 'block',
+display: 'inline-block',
+width: '40%',
 
 
 },
-button:{
-    padding: '1em',
-    background: 'rgba(255, 248, 248, 0.7) ',
-    
-    fontSize: '0.7em',
-    borderRadius: '10px',
-    color: 'black',
-    textDecoration: 'none',
 
-},
 
 
 }))
@@ -120,9 +89,28 @@ function Cardscontato() {
     <div className={classes.cards}>
 
         
-        <div className={classes.texto}><p>Acomodações <br></br>
-        <span className={classes.description}>O espaço possuí três quartos, Casa Beija-flor, Arara Verde e Arara Privê, cada um com particularidades que enriquecem sua experiência</span></p>
-        <a className={classes.button} href='/quartos'> Conheça os quartos </a> 
+        <div className={classes.texto}>
+            <p>Contato <br></br>
+                    <span className={classes.description}>Endereço<br></br><br></br>
+                                Estrada Francisco Thomaz dos Santos, 390
+                                Pântano do Sul, CEP 88067-700
+                                Florianópolis - Santa Catarina - Brasil
+                    </span>
+
+                    <span className={classes.description}>Email<br></br><br></br>
+
+                                maurojaymes@terra.com.br<br></br><br></br>
+                                Telefone<br></br> <br></br>
+                                Móvel e WhatsApp: +55 48 99206-1471
+                    </span>
+            </p>
+            <p>
+                Redes Sociais <br></br>
+                <i className="topIconCon fa-brands fa-facebook"></i>
+        <a href="https://www.instagram.com/arunaespacoregenerativo" target="_blank" rel="noreferrer" ><i className="topIconCon fa-brands fa-instagram"></i></a>
+        <i className="topIconCon fa-brands fa-airbnb"></i>
+
+            </p>
         </div>
         
     </div>
